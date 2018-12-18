1bf5638d58d6c5848f2189208f141b3dc090a38d
Linh Do <luxiliph@gmail.com>
2018-12-18 20:21:40 +0100

    display correct and wrong turns works

--- VibrationWithNavigationUnity/Assets/Scripts/UserDataScript.cs
+++ VibrationWithNavigationUnity/Assets/Scripts/UserDataScript.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class UserDataScript : MonoBehaviour
 {
-    public int correctTurns;
     public int wrongTurns;
+    public int correctTurns;    
+    public GameObject wrongTurnsNumberText;
+    public GameObject correctTurnsNumberText;
     [Tooltip("Select Vibration Method: \n" +
         "1 - One-Handed \n" +
         "2 - Two-Handed"
     )]
     public int vibrationMethod = 1;
 
-    public void addCorrectTurn()
-    {
-        correctTurns++;
-    }
-
     public void addWrongTurn()
     {
         wrongTurns++;
+        wrongTurnsNumberText.GetComponent<UnityEngine.UI.Text>().text = wrongTurns.ToString();
+    }
+
+    public void addCorrectTurn()
+    {
+        correctTurns++;
+        correctTurnsNumberText.GetComponent<UnityEngine.UI.Text>().text = correctTurns.ToString();
     }
 
     public int getVibrationMethod()
