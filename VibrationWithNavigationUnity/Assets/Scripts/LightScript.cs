92f781ad7ec40f730887088999fe4938b1c18ce8
Linh Do <luxiliph@gmail.com>
2019-01-08 00:38:21 +0100

    finish implementing lights

--- VibrationWithNavigationUnity/Assets/Scripts/LightScript.cs
+++ VibrationWithNavigationUnity/Assets/Scripts/LightScript.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 
 public class LightScript : MonoBehaviour
 {
-    Material material;
-    // Start is called before the first frame update
-    void Start()
+    public Material redLight;
+    public Material greenLight;
+
+    public void blinkRight_onehanded()
     {
-        
+        StartCoroutine(enumRight_onehanded());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void blinkLeft_onehanded()
     {
-        
+        StartCoroutine(enumLeft_onehanded());
     }
+
+    public void blink_twoHanded()
+    {
+        StartCoroutine(enum_twoHanded());
+    }
+
+    IEnumerator enumRight_onehanded()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+    }
+
+    IEnumerator enumLeft_onehanded()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+    }
+
+    IEnumerator enum_twoHanded()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = greenLight;
+        yield return new WaitForSeconds(.5f);
+        gameObject.GetComponent<MeshRenderer>().material = redLight;
+    }
+
 }
--- VibrationWithNavigationUnity/Assets/Scripts/TurnTriggerToArduinoScript.cs
+++ VibrationWithNavigationUnity/Assets/Scripts/TurnTriggerToArduinoScript.cs
@@ -7,9 +7,14 @@ public class TurnTriggerToArduinoScript : MonoBehaviour
 
     //public SerialController serialController;
     private int vibrationMethodNumber = 1;
+    private GameObject leftLight;
+    private GameObject rightLight;
+
 
     void Start()
     {
+        leftLight = GameObject.Find("LeftLight");
+        rightLight = GameObject.Find("RightLight");
         //serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
     }
 
@@ -44,21 +49,26 @@ public class TurnTriggerToArduinoScript : MonoBehaviour
 
     private void vibrateLeft_onehanded()
     {
+        leftLight.GetComponent<LightScript>().blinkLeft_onehanded();
         //serialController.SendSerialMessage("V");
     }
 
     private void vibrateRight_onehanded()
     {
+        rightLight.GetComponent<LightScript>().blinkRight_onehanded();
+
         //serialController.SendSerialMessage("B");
     }
 
     private void vibrateLeft_twohanded()
     {
+        leftLight.GetComponent<LightScript>().blink_twoHanded();
         //serialController.SendSerialMessage("N");
     }
 
     private void vibrateRight_twohanded()
     {
+        rightLight.GetComponent<LightScript>().blink_twoHanded();
         //serialController.SendSerialMessage("M");
     }
 }
