1b2be68879c708d381d354b1af4b3e08330602e3
Linh <linh-do@gmx.de>
2019-01-08 17:29:52 +0100

    Bug Fixing in all Maps

--- VibrationWithNavigationUnity/Assets/Standard Assets/Vehicles/Car/Scripts/WheelEffects.cs
+++ VibrationWithNavigationUnity/Assets/Standard Assets/Vehicles/Car/Scripts/WheelEffects.cs
@@ -8,7 +8,7 @@ namespace UnityStandardAssets.Vehicles.Car
     {
         public Transform SkidTrailPrefab;
         public static Transform skidTrailsDetachedParent;
-        public ParticleSystem skidParticles;
+        //public ParticleSystem skidParticles;
         public bool skidding { get; private set; }
         public bool PlayingAudio { get; private set; }
 
@@ -20,16 +20,18 @@ namespace UnityStandardAssets.Vehicles.Car
 
         private void Start()
         {
-            skidParticles = transform.root.GetComponentInChildren<ParticleSystem>();
+            //skidParticles = transform.root.GetComponentInChildren<ParticleSystem>();
 
+            /*
             if (skidParticles == null)
             {
                 Debug.LogWarning(" no particle system found on car to generate smoke particles", gameObject);
             }
             else
             {
-                skidParticles.Stop();
+                //skidParticles.Stop();
             }
+            */
 
             m_WheelCollider = GetComponent<WheelCollider>();
             m_AudioSource = GetComponent<AudioSource>();
@@ -44,8 +46,8 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public void EmitTyreSmoke()
         {
-            skidParticles.transform.position = transform.position - transform.up*m_WheelCollider.radius;
-            skidParticles.Emit(1);
+            //skidParticles.transform.position = transform.position - transform.up*m_WheelCollider.radius;
+            //skidParticles.Emit(1);
             if (!skidding)
             {
                 StartCoroutine(StartSkidTrail());
